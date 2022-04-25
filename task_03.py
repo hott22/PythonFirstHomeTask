ea93d6cc3b2fe4c68eae3f591c0662ef94b7d306
Bobby <mif210210@gmail.com>
2022-04-25 14:58:10 +0300

    edit task_03.py

--- task_03.py
+++ task_03.py
@@ -4,22 +4,25 @@
 from random import randint
 
 n = int(input('Количество чисел в последовательности: '))
-my_list = []
 
-def Sequence(data, number):
+
+def Sequence(number):
+    data = []
     for i in range(1, number+1):
         data.append(randint(1, 20))
 
-    return print(data)
+    return data
 
-Sequence(my_list, n)
+my_list = Sequence(n)
+print(my_list)
 
 def newSequence(data):
-    newList = []
+    newData = []
     for i in data:
-        if i not in newList:
-            newList.append(i)
+        if i not in newData:
+            newData.append(i)
             
-    return print(newList)
+    return newData
 
 new_list = newSequence(my_list)
+print(new_list)
