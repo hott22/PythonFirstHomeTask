d0e193877617393f1bb2c613b88f31e7ce52b7cd
Bobby <mif210210@gmail.com>
2022-04-27 23:02:15 +0300

    edit task_03.py

--- task_03.py
+++ task_03.py
@@ -13,16 +13,19 @@ def Sequence(number):
 
     return data
 
+
 my_list = Sequence(n)
 print(f'Последовательность чисел: {my_list}')
+
 
 def newSequence(data):
     newData = []
     for i in data:
-        if i not in newData and data.count(i)==1:
+        if data.count(i) == 1:
             newData.append(i)
-            
+
     return newData
+
 
 new_list = newSequence(my_list)
 print(f'Последовательность неповторяющихся чисел: {new_list}')
