de209caa1ea18b8287ab70e8e293dfdf7a91a518
Bobby <mif210210@gmail.com>
2022-05-12 10:30:12 +0300

    edit task_03

--- task_03.py
+++ task_03.py
@@ -1,26 +1,16 @@
 # Задайте последовательность чисел.
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
-from random import randint
+import random 
+
 
 n = int(input('Количество чисел в последовательности: '))
-
-
-def Sequence(number):
-    data = []
-    for i in range(1, number+1):
-        data.append(randint(1, 10))
-    return data
-
-
-my_list = Sequence(n)
-print(f'Последовательность чисел: {my_list}')
-
+la = [random.randint(-10, 10) for i in range(n)]
+print(f'Последовательность чисел: {la}')
 
 def newSequence(data):
     newData = [i for i in data if data.count(i) == 1]
     return newData
 
-
-new_list = newSequence(my_list)
+new_list = newSequence(la)
 print(f'Последовательность неповторяющихся чисел: {new_list}')
